d69844dba85dfe5a8c31a4ac01a14e32986d0ff8
Kazuki Hirata <family.computer59@gmail.com>
2017-06-29 09:07:57 +0000

    Modify image to Users 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
 
 protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :role, :name, :image) }
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :role, :name) }
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password, :role, :name, :image) }
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -30,28 +30,4 @@ class User < ActiveRecord::Base
   
   mount_uploader :image, ImageUploader
     
-  # ------------------------------------------------------------------
-  # Validations
-  # ------------------------------------------------------------------
-  validate def check_image_dimensions
-    if geometry[:width] < 400 || geometry[:height] < 400
-      errors.add :image, 'Please upload images larger than 400 x 400 pixels'
-    end
-  end
-  # ------------------------------------------------------------------
-  # Public Instance Methods
-  # ------------------------------------------------------------------
-  def geometry
-    @geometry ||= _geometry
-  end
-  private
-  # ------------------------------------------------------------------
-  # Private Instance Methods
-  # ------------------------------------------------------------------
-  def _geometry
-    if image.file and File.exists?(image.file.file)
-      img = ::Magick::Image::read(image.file.file).first
-      { width: img.columns, height: img.rows }
-    end
-  end
 end
