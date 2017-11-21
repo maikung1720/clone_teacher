1817cfe7b3073ba876c00e4fa3cd796e73fd1e17
maikung1720 <maikung1720@gmail.com>
2017-11-21 13:01:39 +0000

    Cloneteacher2

--- app/controllers/api/box_opener_controller.rb
+++ app/controllers/api/box_opener_controller.rb
@@ -6,25 +6,27 @@ class Api::BoxOpenerController < ApplicationController
       @users = User.where("card_no = ?", box_opener_params[:card_no])
       @users.each do |user|
           if user.role.eql?("teacher") then
-              str = JSON.generate({ "status" => "approval" , "card_no" => box_opener_params[:card_no] })
+               str = JSON.generate({ "status" => "approval" , "rack_no" => "all" })
           else
               @rentals = Rental.where("status = ? or status = ?", "approval","lending").where("User_id= ? and rental_date <= ? and due_date >= ? ", user.id, Time.parse("00:00"), Time.parse("00:00"))
               logger.debug Time.parse("00:00")
               @rentals.each do |rental|
-                  str = JSON.generate({ "status" => rental.status , "card_no" => box_opener_params[:card_no], "rack_no" => rental.rack_no })
-                  if rental.status.eql?("approval") then
-                    rental.status = "lending"
-                  else
-                    rental.rental_details.each do |i|
-                      labwares = Labware.where("name = ?",i.labware.name)
-                      labwares.each do |labware|
-                        labware.circulation -= i.quantity
-                        labware.update(labware_params)
-                      end
+                    if rental.User_id.eql?(user.id) then
+                        str = JSON.generate({ "status" => rental.status , "card_no" => box_opener_params[:card_no], "rack_no" => rental.rack_no })
+                        if rental.status.eql?("approval") then
+                            rental.status = "lending"
+                        else
+                            rental.rental_details.each do |i|
+                            labwares = Labware.where("name = ?",i.labware.name)
+                                labwares.each do |labware|
+                                    labware.circulation -= i.quantity
+                                    labware.update(labware_params)
+                                end
+                            end
+                            rental.status = "returned"
+                        end
+                        rental.update(rental_params)
                     end
-                    rental.status = "returned"
-                  end
-                  rental.update(rental_params)
               end
           end
           render :json => str and return
