65d59a2a6b81599c198a78bf9809bd0902d422d0
Kazuki Hirata <family.computer59@gmail.com>
2018-01-11 14:04:11 +0000

    Check

--- app/controllers/api/box_opener_controller.rb
+++ app/controllers/api/box_opener_controller.rb
@@ -6,7 +6,7 @@ class Api::BoxOpenerController < ApplicationController
       @users = User.where("card_no = ?", box_opener_params[:card_no])
       @users.each do |user|
           if user.role.eql?("teacher") then
-               str = JSON.generate({ "status" => "approval" , "rack_no" => "all" })
+               str = JSON.generate({"rack_no" => "all" })
           else
             #   @rentals = Rental.where("status = ? or status = ?", "approval","lending").where("User_id= ? and rental_date <= ? and due_date >= ? ", user.id, Time.parse("00:00"), Time.parse("00:00"))
               @rentals = Rental.where("status = ? or status = ?", "approval","lending")
