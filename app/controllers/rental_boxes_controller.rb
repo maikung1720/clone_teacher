ffe9a01cdd9eaba9444077289cdb7f35776c72a7
Kazuki Hirata <family.computer59@gmail.com>
2018-07-21 01:34:50 +0000

    test

--- app/controllers/rental_boxes_controller.rb
+++ app/controllers/rental_boxes_controller.rb
@@ -1,6 +1,7 @@
 class RentalBoxesController < ApplicationController
   before_action :authenticate_user!
   def show
+    logger.debug "logger.debug 3"
     @rental_details = current_rental.rental_details
   end
 end
